cb053926591e60fd4fae86243ef11717162bec5e
Nguyễn Quang Huy <huynq12@topica.edu.vn>
2019-03-18 11:26:50 +0700

    smothly UX

--- docker-build-update.bat
+++ docker-build-update.bat
@@ -2,7 +2,7 @@
 #docker login docker.topica.vn -u username -p password
 
 #build inheritance
-docker build -f Dockerfile-inheritance -t docker.topica.vn/icovn-rancher-status:1.0.2-SNAPSHOT .
+docker build -f Dockerfile-inheritance -t docker.topica.vn/icovn-rancher-status:1.0.3-SNAPSHOT .
 
 #push
-docker push docker.topica.vn/icovn-rancher-status:1.0.2-SNAPSHOT
+docker push docker.topica.vn/icovn-rancher-status:1.0.3-SNAPSHOT
